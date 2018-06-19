26474fa34a6ef0761a1cfbb17d28ac4eb31f5675
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-19 18:02:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function theBeatlesPlay(musicians, instruments)
 {
   var array = [];
   
-  for(let i = 0; l = musicians.length, i < l; i++)
+  for(let i = 0, l = musicians.length; i < l; i++)
   {
-    array.push('${musicians} plays the {instruments}')
+    array.push('${musicians} plays the ${instruments}')
   }
   return array
 }
